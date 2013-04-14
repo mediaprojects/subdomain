32125df14c1d9eede7365070a39b08d74219e801
pocesar <gahgneh+github@gmail.com>
2013-04-14 16:24:45 -0300

    Non static changes

--- classes/Subdomain/Request.php
+++ classes/Subdomain/Request.php
@@ -5,46 +5,159 @@ class Subdomain_Request extends Kohana_Request {
 	/**
 	 * @var  string  request Subdomain
 	 */
-	public static $subdomain;
-			
-	public static function factory($uri = TRUE, $client_params = array(), $allow_external = TRUE, $injected_routes = array()){
-		self::$subdomain = Request::catch_subdomain() ;
+	public $subdomain;
 
-		return parent::factory($uri, $client_params, $allow_external, $injected_routes) ;
+	public function __construct($uri, $client_params = array(), $allow_external = TRUE, $injected_routes = array())
+	{
+		$this->subdomain = Request::catch_subdomain();
+
+		parent::__construct($uri, $client_params, $allow_external, $injected_routes);
 	}
-	
-	public static function catch_subdomain($base_url = NULL, $host = NULL) {
-		if($base_url === NULL) 
+
+	/**
+	 * @param string $domain Pass $_SERVER['SERVER_NAME'] here
+	 * @param bool $debug
+	 *
+	 * @debug bool $debug
+	 * @return string
+	 */
+	public static function get_domain($domain, $debug = false)
+	{
+		$original = $domain = strtolower($domain);
+
+		if (filter_var($domain, FILTER_VALIDATE_IP))
 		{
-			$base_url = parse_url(Kohana::$base_url, PHP_URL_HOST);
+			return $domain;
 		}
-		
-		if($host === NULL) 
+
+		$debug ? print('<strong style="color:green">&raquo;</strong> Parsing: ' . $original) : false;
+
+		$arr = array_slice(array_filter(explode('.', $domain, 4), function ($value)
 		{
-			if(php_sapi_name() == 'cli' AND empty($_SERVER['REMOTE_ADDR'])) 
+			return $value !== 'www';
+		}), 0); //rebuild array indexes
+
+		if (count($arr) > 2)
+		{
+			$count = count($arr);
+			$_sub = explode('.', $count === 4 ? $arr[3] : $arr[2]);
+
+			$debug ? print(" (parts count: {$count})") : false;
+
+			if (count($_sub) === 2) // two level TLD
+			{
+				$removed = array_shift($arr);
+				if ($count === 4) // got a subdomain acting as a domain
+				{
+					$removed = array_shift($arr);
+				}
+				$debug ? print("<br>\n" . '[*] Two level TLD: <strong>' . join('.', $_sub) . '</strong> ') : false;
+			}
+			elseif (count($_sub) === 1) // one level TLD
+			{
+				$removed = array_shift($arr); //remove the subdomain
+
+				if (strlen($_sub[0]) === 2 && $count === 3) // TLD domain must be 2 letters
+				{
+					array_unshift($arr, $removed);
+				}
+				else
+				{
+					// non country TLD according to IANA
+					$tlds = array(
+						'aero',
+						'arpa',
+						'asia',
+						'biz',
+						'cat',
+						'com',
+						'coop',
+						'edu',
+						'gov',
+						'info',
+						'jobs',
+						'mil',
+						'mobi',
+						'museum',
+						'name',
+						'net',
+						'org',
+						'post',
+						'pro',
+						'tel',
+						'travel',
+						'xxx',
+					);
+
+					if (count($arr) > 2 && in_array($_sub[0], $tlds) !== false) //special TLD don't have a country
+					{
+						array_shift($arr);
+					}
+				}
+				$debug ? print("<br>\n" . '[*] One level TLD: <strong>' . join('.', $_sub) . '</strong> ') : false;
+			}
+			else // more than 3 levels, something is wrong
+			{
+				for ($i = count($_sub); $i > 1; $i--)
+				{
+					$removed = array_shift($arr);
+				}
+				$debug ? print("<br>\n" . '[*] Three level TLD: <strong>' . join('.', $_sub) . '</strong> ') : false;
+			}
+		}
+		elseif (count($arr) === 2)
+		{
+			$arr0 = array_shift($arr);
+
+			if (strpos(join('.', $arr), '.') === false
+			&& in_array($arr[0], array('localhost', 'test', 'invalid')) === false
+			) // not a reserved domain
+			{
+				$debug ? print("<br>\n" . 'Seems invalid domain: <strong>' . join('.', $arr) . '</strong> re-adding: <strong>' . $arr0 . '</strong> ') : false;
+				// seems invalid domain, restore it
+				array_unshift($arr, $arr0);
+			}
+		}
+
+		$debug ? print("<br>\n" . '<strong style="color:gray">&laquo;</strong> Done parsing: <span style="color:red">' . $original . '</span> as <span style="color:blue">' . join('.', $arr) . "</span><br>\n") : false;
+
+		return join('.', $arr);
+	}
+
+	public static function catch_subdomain($base_url = NULL, $host = NULL)
+	{
+		if ($base_url === NULL)
+		{
+			$base_url = self::get_domain($_SERVER['SERVER_NAME']);
+		}
+
+		if ($host === NULL)
+		{
+			if (php_sapi_name() == 'cli' AND empty($_SERVER['REMOTE_ADDR']))
 			{
 				return FALSE;
 			}
-			
+
 			$host = $_SERVER['HTTP_HOST'];
 		}
-		
-		if(empty($base_url) OR empty($host) OR in_array($host, Route::$localhosts) OR Valid::ip($host)) 
+
+		if (empty($base_url) OR empty($host) OR in_array($host, Route::$localhosts) OR Valid::ip($host))
 		{
 			return FALSE;
 		}
-		
+
 		$sub_pos = (int)strpos($host, $base_url) - 1;
-		
-		if($sub_pos > 0) 
+
+		if ($sub_pos > 0)
 		{
 			$subdomain = substr($host, 0, $sub_pos);
-			
-			if(! empty($subdomain)) {
-				return $subdomain ;
+
+			if (!empty($subdomain))
+			{
+				return $subdomain;
 			}
 		}
-		
-		return Route::SUBDOMAIN_EMPTY;
+
+		return FALSE;
 	}
 }
--- classes/Subdomain/Route.php
+++ classes/Subdomain/Route.php
@@ -2,54 +2,75 @@
 
 class Subdomain_Route extends Kohana_Route {
 
-    const SUBDOMAIN_WILDCARD = '*', SUBDOMAIN_EMPTY = '' ;
-
-	public static $default_subdomains = array(self::SUBDOMAIN_EMPTY, 'www');
+	const SUBDOMAIN_WILDCARD = '*';
 
 	/**
 	 * @var  string  route SUBDOMAIN
 	 */
-	protected $_subdomain;
-	
-	public function __construct($uri = NULL, $regex = NULL) {
+	protected $_subdomain = array();
+	protected $has_subdomain = FALSE;
+	protected $restrict = FALSE;
+
+	public function __construct($uri = NULL, $regex = NULL)
+	{
 		parent::__construct($uri, $regex);
-		
-        // Set default subdomains in this route rule
-		$this->_subdomain = self::$default_subdomains;
+
+		// Set default subdomains in this route rule
+		$this->has_subdomain = FALSE;
+		$this->restrict = FALSE;
+		$this->_subdomain = array();
 	}
 
 
-    /**
-     * Set one or more subdomains to execute this route
-     *
+	/**
+	 * Set one or more subdomains to execute this route
+	 *
 	 *     Route::set('default', '(<controller>(/<action>(/<id>)))')
-     *         ->subdomains(array(Route::SUBDOMAIN_EMPTY, 'www1', 'foo', 'bar'))
+	 *         ->subdomains(array(Route::SUBDOMAIN_EMPTY, 'www1', 'foo', 'bar'))
 	 *         ->defaults(array(
 	 *             'controller' => 'welcome',
 	 *         ));
-     *
-	 * @param   array    name(s) of subdomain(s) to apply in route
-     * @return Route
-     */      
-    public function subdomains(array $name) {
-        $this->_subdomain = $name;
+	 *
+	 * @param   array  $name  name(s) of subdomain(s) to apply in route
+	 *
+	 * @param bool $restrict
+	 *
+	 * @return Route
+	 */
+	public function subdomains(array $name, $restrict = FALSE)
+	{
+		$this->_subdomain = $name;
+		$this->has_subdomain = true;
+		$this->restrict = (bool)$restrict;
 
-        return $this;
-    }
-	
-	public function matches(Request $request, $subdomain = NULL) {
-		$subdomain = (!isset($subdomain) OR $subdomain === NULL) ? Request::$subdomain : $subdomain;
-		
-		if($subdomain === FALSE) 
+		return $this;
+	}
+
+	public function matches(Request $request, $subdomain = NULL)
+	{
+		$subdomain = (!isset($subdomain) OR $subdomain === NULL) ? $request->subdomain : $subdomain;
+
+
+		if ($this->has_subdomain === FALSE)
 		{
-			$subdomain = self::SUBDOMAIN_EMPTY;
+			return parent::matches($request);
 		}
-		
-		if(in_array(self::SUBDOMAIN_WILDCARD, $this->_subdomain) OR in_array($subdomain, $this->_subdomain)) 
+
+		if ($this->restrict === TRUE)
 		{
-			return parent::matches($request) ;
+			if (!empty($subdomain) && (in_array(self::SUBDOMAIN_WILDCARD, $this->_subdomain) || in_array($subdomain, $this->_subdomain)))
+			{
+				return parent::matches($request);
+			}
 		}
-		
+		else
+		{
+			if (in_array(self::SUBDOMAIN_WILDCARD, $this->_subdomain) || in_array($subdomain, $this->_subdomain))
+			{
+				return parent::matches($request);
+			}
+		}
+
 		return FALSE;
 	}
 }
